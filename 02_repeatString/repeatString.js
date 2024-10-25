e8e16521545f5b12935f33be8709c2c62fffe801
saurabhsensai <saurabhnale9@gmail.com>
2024-10-25 10:40:49 +0530

    Solved this exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    let final = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            final = final.concat(str);
 
+        }
+
+        return final;  
+    }
+
+    return "ERROR";
+    
+    
 };
 
 // Do not edit below this line
